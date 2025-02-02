789b4e5c775af0b20ce962a1f766000afdb4ae2c
livbrandao <liviatoledo27@gmail.com>
2025-02-01 23:42:43 -0300

    feat(configs): adiciona configurações iniciais do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.ts"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "spotify-green": "#1db954",
+        "spotify-black": "#121212",
+        "spotify-gray": "#b3b3b3",
+        "spotify-light-black": "#242424",
+        "spotify-hover": "#252525",
+      },
+      spacing: {
+        "sidebar-width": "300px",
+      },
+      fontFamily: {
+        "dm-sans": ["DM Sans", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
