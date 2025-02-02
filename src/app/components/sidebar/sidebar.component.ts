6c01a04e2228b8de17f73d083a6ac6dcad342e4a
livbrandao <liviatoledo27@gmail.com>
2025-02-01 23:44:04 -0300

    feat(sidebar): adiciona componente sidebar

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent {}
