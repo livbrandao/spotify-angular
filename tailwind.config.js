2721fe3cb35524ee0a063d81e93167eb9861139b
livbrandao <liviatoledo27@gmail.com>
2025-02-03 00:24:22 -0300

    fix(style): ajuste nos estilos globais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.ts"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       colors: {
@@ -9,6 +9,10 @@ module.exports = {
         "spotify-gray": "#b3b3b3",
         "spotify-light-black": "#242424",
         "spotify-hover": "#252525",
+        "spotify-border": "#878787",
+        "spotify-whithe": "#fff",
+        "bg-icon": "#131313",
+        "bg-arrow-color": "rgba(0, 0, 0, 7)",
       },
       spacing: {
         "sidebar-width": "300px",
