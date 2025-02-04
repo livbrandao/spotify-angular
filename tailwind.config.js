db80e5a4f73c9d36117e11f531bcbe5e2e8702cb
livbrandao <liviatoledo27@gmail.com>
2025-02-03 22:56:37 -0300

    fix(libs): update versão tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
       colors: {
         "spotify-green": "#1db954",
         "spotify-black": "#121212",
-        "spotify-gray": "#b3b3b3",
         "spotify-light-black": "#242424",
+        "spotify-gray": "#b3b3b3",
+        "spotify-light-gray": "#a7a7a7",
         "spotify-hover": "#252525",
         "spotify-border": "#878787",
         "spotify-whithe": "#fff",
-        "bg-icon": "#131313",
-        "bg-arrow-color": "rgba(0, 0, 0, 7)",
+        "spotify-icon": "#131313",
+        "spotify-arrow-color": "rgba(0, 0, 0, 7)",
       },
       spacing: {
         "sidebar-width": "300px",
