c6b3ab4144a1c798e032786ab65c8946909086e3
livbrandao <liviatoledo27@gmail.com>
2025-02-05 01:17:34 -0300

    feat(service): cria service para requisição http

--- src/app/services/artist.service.ts
+++ src/app/services/artist.service.ts
@@ -6,12 +6,12 @@ import { Artist } from '../interfaces/artist.interface';
 @Injectable({
   providedIn: 'root',
 })
-export class ArtistService {
+export class ArtistsService {
   private apiUrl = 'http://localhost:3000/artists';
 
   constructor(private http: HttpClient) {}
 
-  searchArtists(searchTerm: string): Observable<Artist[]> {
-    return this.http.get<Artist[]>(`${this.apiUrl}?q=${searchTerm}`);
+  getArtists(): Observable<Artist[]> {
+    return this.http.get<Artist[]>(this.apiUrl);
   }
 }
